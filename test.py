9aaeaf44d4f58d019c0f795780c9d71693b820f5
Kamil-Kam <116226497+Kamil-Kam@users.noreply.github.com>
2023-05-31 20:37:43 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-headers = ['date', '"media"', '"medium"', '"daypart"', '"audiocode"', '"duration"', '"grp_a_18_24"', '"grp_a_20_30"', '"cost"']
-
-# Usuń cudzysłowy z elementów listy
-headers = [header.replace('"', '') for header in headers]
-
-print(headers)
